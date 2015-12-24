9e2480253f316c53efbe57b20336fe1d92a6e86a
sysrich <sysrich@linux.com>
2015-12-24 14:35:30 +0000

    Debug

--- opensuse/default-dconf.sh
+++ opensuse/default-dconf.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+echo "Initial bus address"
+echo $DBUS_SESSION_BUS_ADDRESS
 # Source the dbus session bus
 . /home/ilmehtar/.dbus/session-bus/*
+
+echo "Imported bus address"
+echo $DBUS_SESSION_BUS_ADDRESS
 
 # Import default dconf settings
 /usr/bin/dconf load / < /home/ilmehtar/.default-settings.dconf
