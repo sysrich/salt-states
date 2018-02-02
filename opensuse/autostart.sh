59d108ae30921f43e51a2dcbc3e04bad438adc04
Richard Brown <RBrownCCB@opensuse.org>
2018-02-02 18:25:33 +0100

    Update autostart.sh

--- opensuse/autostart.sh
+++ opensuse/autostart.sh
@@ -7,7 +7,7 @@
 /usr/bin/terminator -H -f &
 
 # Notify what's going on
-zenity --info --text "Backing up Docs/Pictures/Home to k2so\n\nPlease ensure network is connected then click Ok" --height=100
+zenity --info --text "Backing up Docs/Pictures/Home to k2so\n\nPlease ensure network is connected then click Ok" --width=500
 # Run regular csyncs as ilmehtar
 /usr/bin/csync /home/ilmehtar/Documents sftp://ilmehtar@k2so.dyn.rootco.de:8282/backups/home/Documents
 /usr/bin/csync /home/ilmehtar/Pictures sftp://ilmehtar@k2so.dyn.rootco.de:8282/backups/home/Pictures
