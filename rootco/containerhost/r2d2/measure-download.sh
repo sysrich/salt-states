4e698c773afb7282e596b65be84a35b1370485b8
Richard Brown <rbrownccb@opensuse.org>
2021-09-22 16:40:33 +0200

    Measure using iperf

---
+++ rootco/containerhost/r2d2/measure-download.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+podman run --rm registry.opensuse.org/home/rbrownsuse/containers/containers/iperf:latest /usr/bin/iperf3 -b 1000M -R -P 10 -t 1 -c d0.rootco.de | grep "\[SUM\]" | grep receiver | awk '{print $6}'
